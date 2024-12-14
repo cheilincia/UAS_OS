300920e4d18741931c1ee7205be2925605eb58cb
cheilincia <cheilinciamenggana@gmail.com>
2024-12-14 15:13:18 +0900

    percobaan ke sekian

---
+++ konversi.c
@@ -0,0 +1,22 @@
+ void dolar ()
+{
+float rupiah,dolar;
+
+printf ("masukkan jumlah rupiah: ");
+scanf ("%f" ,&rupiah);
+dolar = rupiah/15943;
+printf ("\t nilai konversi rupiah ke dolar adalah : $ %f\n", dolar);
+
+}
+
+void rupiah ()
+{
+float rupiah,dolar;
+
+printf ("masukkan jumlah dolar: ");
+scanf ("%f" ,&dolar);
+rupiah  = dolar*15943;
+printf ("\t nilai konversi rupiah ke dolar adalah :Rp %f\n", rupiah);
+
+}
+
